c9ee1a831cd989dd63b2d97bed446ab894b6c0ca
Christopher0985 <ckamowatimwa@gmail.com>
2023-02-18 21:26:37 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
  /**
-  *main - A program that prints the size of various computer types
+  *main - Entry point
   *Return: Always 0 (Success)
   */
 int main(void)
@@ -12,4 +12,3 @@ int main(void)
 	printf("Size of a float: %lu byte(s)", sizeof(float));
 	return (0);
 }
-
